7a8944b28bd3bcca29b7d468740cc40d8d978d91
unknown <github_cloudlabsuser_2103@cloudlabsaiuser.com>
2024-06-18 12:06:36 +0000

    setti

---
+++ appi.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/')
+def disk_usage():
+    # Run the PowerShell script and capture the output
+    output = subprocess.check_output(['powershell.exe', '.\\monit.ps1'], shell=True)
+
+    # Decode the output and replace newline characters with HTML breaks
+    output = output.decode('utf-8').replace('\n', '<br>')
+
+    return output
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
